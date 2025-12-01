50f631ff142127251a30c5016c34aad43fc22afe
fercoiman <fcoiman@frba.utn.edu.ar>
2025-11-30 22:30:53 -0300

    agrego base de datos

--- src/daos/models/palabra.model.js
+++ src/daos/models/palabra.model.js
@@ -2,31 +2,38 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const palabraSchema = new Schema({
-  palabra: {
-    type: String,
-    required: true,
-    trim: true,
+const palabraSchema = new Schema(
+  {
+    palabra: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+      transform: (_, ret) => {
+        ret.id = ret.id ?? ret._id?.toString();
+        ret.timestamp =
+          ret.timestamp instanceof Date
+            ? ret.timestamp.getTime()
+            : ret.timestamp;
+
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
 
 palabraSchema.virtual("id").get(function () {
   return this._id.toString();
-});
-
-palabraSchema.set("toJSON", {
-  virtuals: true,
-  transform: (_, ret) => {
-    ret.id = ret.id;
-    ret.timestamp =
-      ret.timestamp instanceof Date ? ret.timestamp.getTime() : ret.timestamp;
-    delete ret._id;
-    delete ret.__v;
-  },
 });
 
 const PalabraModel = model("Palabra", palabraSchema);
